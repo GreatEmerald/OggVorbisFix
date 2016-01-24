7ca087b2c2fb105b9068f96c0a035bc11592ed84
Dainius Masiliūnas <pastas4@gmail.com>
2016-01-24 23:38:48 +0100

    Add the code

---
+++ oggvorbisfix.c
@@ -0,0 +1,55 @@
+// Corrupted Ogg Vorbis file fixer
+
+/* If you have an OGG Vorbis file whose header (up to the first ~48 bytes) is
+ * corrupt, use this program to recover it. If the corruption does not reach the
+ * 22nd byte, this recovers the file completely. Else, after running the tool,
+ * you also need to run `ffmpeg -i input.ogg -c:a copy output.ogg` in order to
+ * recalculate the CRC checksums. */
+
+/* Copyright 2016 GreatEmerald
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. */
+
+#include <stdio.h>
+
+char* OggS = "OggS\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00";
+char* Zeroes = "\x00\x00\x00\x00";
+char* HeaderEnd = "\x01\x1E\x01vorbis\x00\x00\x00\x00\x02\x44\xAC\x00\x00\x00\x00\x00\x00";
+
+int main(int argc, char *argv[])
+{
+    int count;
+    FILE* ogg_file;
+    char* buffer;
+
+    for (count = 1; count < argc; count++)
+    {
+        printf("Processing OGG Vorbis file %d: %s\n", count, argv[count]);
+        ogg_file=fopen(argv[count],"rb+");
+        // Write header (includes start of file bit)
+        fwrite(OggS, 14, 1, ogg_file);
+        // Get first stream GUID
+        fseek(ogg_file, 72, SEEK_SET);
+        fread(&buffer, 4, 1, ogg_file);
+        // Set it as the first stream of the file
+        fseek(ogg_file, 14, SEEK_SET);
+        fwrite(&buffer, 4, 1, ogg_file);
+        // Write zeroes to overwrite garbage
+        fseek(ogg_file, 18, SEEK_SET);
+        fwrite(Zeroes, 4, 1, ogg_file);
+        // Skip next four bytes (CRC checksum), will be recalculated by FFmpeg if needed
+        fseek(ogg_file, 26, SEEK_SET);
+        // Write rest of header
+        fwrite(HeaderEnd, 22, 1, ogg_file);
+        fclose(ogg_file);
+    }
+    return 0;
+}
